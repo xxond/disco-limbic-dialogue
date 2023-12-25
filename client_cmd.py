20d7fd5fbb8a50d56f5f00ca90d331cc81271209
xxond <sasha2000sm@gmail.com>
2023-12-25 18:40:55 +0300

    changing lora model to merged one

--- client_cmd.py
+++ client_cmd.py
@@ -6,7 +6,9 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from utils.dataset import template
 
 # Model setup
+model_type = 'phi'
 device = 'cuda'
+
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
@@ -14,13 +16,11 @@ bnb_config = BitsAndBytesConfig(
     bnb_4bit_compute_dtype=torch.bfloat16
 )
 
-model_type = 'minichat'
+model_type = 'phi'
 if model_type == 'phi':
-    model_id = 'microsoft/phi-2'
-    lora_path = 'lora/disco-limbic-dialogue-phi2-eos/'
-
+    model_path = 'xxond/disco-limbic-dialogue'
     model = AutoModelForCausalLM.from_pretrained(
-        model_id,
+        model_path,
         torch_dtype="auto",
         flash_attn=True,
         flash_rotary=True,
@@ -29,27 +29,26 @@ if model_type == 'phi':
         device_map={'': 0},
         quantization_config=bnb_config
     )
-
 elif model_type == 'minichat':
-    lora_path = 'lora/disco-limbic-dialogue-512/'
-    model_id = 'GeneZC/MiniChat-3B'
-    model = AutoModelForCausalLM.from_pretrained(model_id, use_cache=True,
-                                             device_map="auto",
-                                             quantization_config=bnb_config,
-                                             )
+    model_path = 'model/disco-limbic-dialogue-512'
+    model = AutoModelForCausalLM.from_pretrained(
+        model_path, use_cache=True,
+        device_map="auto",
+        quantization_config=bnb_config,
+    )
+else:
+    raise NotImplementedError
 
-tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=False)
+tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
 if tokenizer.pad_token is None:
     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
     model.resize_token_embeddings(len(tokenizer))
 
-model.config.pad_token_id = tokenizer.pad_token_id
-model.config.eos_token_id = tokenizer.eos_token_id
+# Moving setting eos and pad to generation to remove UserWarning
+# model.config.pad_token_id = tokenizer.pad_token_id
+# model.config.eos_token_id = tokenizer.eos_token_id
 
 model = model.eval()
-lora_model = peft.PeftModel.from_pretrained(model, lora_path,
-                                            adapter_name='loraTrained',
-                                            is_trainable=False)
 
 dialog = [
 ]
@@ -67,6 +66,8 @@ while True:
     input_len = len(model_inputs[0])
     generated_ids = model.generate(input_ids=model_inputs, max_new_tokens=128,
                                 do_sample=True,
+                                pad_token_id=tokenizer.eos_token_id,
+                                eos_token_id=tokenizer.eos_token_id,
                                 temperature=0.7,
                                 repetition_penalty=1.15)
     output = tokenizer.decode(generated_ids[0][input_len:], skip_special_tokens=True)
